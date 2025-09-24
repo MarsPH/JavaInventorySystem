9302078b0487f2002ce082f41a713a769e171341
AegonSnow <sahajswag3500@gmail.com>
2025-09-24 16:24:04 -0400

    refactor: move classes into package for cleaner main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import DAO.ItemDAO;
 import ui.MainMenu;
 import util.DBconnection;
+import  model.Item;
 
 import java.sql.*;
 import java.util.*;
@@ -12,6 +14,7 @@ public class Main {
 
         MainMenu mainMenu = new MainMenu();
        mainMenu.Start();
+       ItemDAO itemDAO= new ItemDAO();
 
         //if any exception is run to, it will be set to false
         boolean running = true;
@@ -19,10 +22,10 @@ public class Main {
         while (running) {
 
             Item item = askUserForItem();// gets the input from the user to set data to the database + validation
-            saveItemToDatabase(item);// set the data from created item from input into the database
+            itemDAO.saveItemToDatabase(item);// set the data from created item from input into the database
             running = askUserToContinue();// asks if the user want to add another item
         }
-        printAllItems();// after the inputs, it shows the table of items
+       // itemDAO.printAllItems();// after the inputs, it shows the table of items
 
     }
 
@@ -55,37 +58,9 @@ public class Main {
 
     }
 
-    public static void saveItemToDatabase(Item item) {
 
-        try (Connection conn = DBconnection.getConnection() ;
-             PreparedStatement pst = conn.prepareStatement("insert into items (name, category, price) values (?, ?, ?)")
-        ) {
-            pst.setString(1, item.getName()); //sets the first ? as the name of the item to == name
-            pst.setString(2, "Weapon");// On default sets this as category
-            pst.setInt(3, item.getValue());// sets the price of the item as the price
-            pst.executeUpdate();//runs the command
-            System.out.println("Connected to database!");
 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    public static void printAllItems() {
-        try (Connection conn = DBconnection.getConnection();
-             Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery(sql)
-        ) {
-            while (rs.next()) {
-                String name = rs.getString("name"); // or column label like "name" - USED Label
-                String price = rs.getString("price");
-                System.out.print(name + " : " + price + "\n");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
     public static boolean askUserToContinue() {
         char charChoiceToContinue = 'c';
